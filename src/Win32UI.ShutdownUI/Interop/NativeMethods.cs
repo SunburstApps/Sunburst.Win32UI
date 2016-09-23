0949f4fe7e32a194c5e3d441660b11c98c35fbfe
William Kent <wjk@sunsol.me>
2016-09-22 23:21:52 -0400

    Migrate Win32UI.ShutdownUI

---
+++ src/Win32UI.ShutdownUI/Interop/NativeMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Win32.UserInterface.Interop
+{
+    internal sealed class NativeMethods
+    {
+        private NativeMethods()
+        {
+            throw new InvalidOperationException();
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool ShutdownBlockReasonCreate(IntPtr hWnd, string reason);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShutdownBlockReasonDestroy(IntPtr hWnd);
+    }
+}
---
+++ src/Win32UI.ShutdownUI/ShutdownUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Win32.UserInterface.Interop;
+
+namespace Microsoft.Win32.UserInterface
+{
+    public static class ShutdownUI
+    {
+        public static void CreateShutdownBlock(Window window, string reason)
+        {
+            bool success = NativeMethods.ShutdownBlockReasonCreate(window.Handle, reason);
+            if (!success) throw new System.ComponentModel.Win32Exception();
+        }
+
+        public static void DestroyShutdownBlock(Window window)
+        {
+            bool success = NativeMethods.ShutdownBlockReasonDestroy(window.Handle);
+            if (!success) throw new System.ComponentModel.Win32Exception();
+        }
+
+        public static void BlockShutdown(Window window, string reason, Action action)
+        {
+            // This is outside the try clause so that any exceptions thrown by it will
+            // be ignored by the finally clause. This prevents a possible second exception
+            // from being thrown if creating the shutdown block failed.
+            CreateShutdownBlock(window, reason);
+
+            try
+            {
+                action();
+            }
+            finally
+            {
+                DestroyShutdownBlock(window);
+            }
+        }
+    }
+}
