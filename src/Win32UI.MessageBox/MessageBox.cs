f686a66ea718f0d54675dbf15c7d4c4a42b930b0
William Kent <wjk@sunsol.me>
2016-09-22 23:04:49 -0400

    Migrate Win32UI.MessageBox

---
+++ src/Win32UI.MessageBox/MessageBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Win32.UserInterface
+{
+    public static class MessageBox
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "MessageBoxW")]
+        public static extern int NativeMessageBox(IntPtr hWnd, string lpText, string lpCaption, uint flags);
+
+        public static MessageBoxButton Show(string text, string caption, MessageBoxFlags flags, Window parent = null)
+        {
+            IntPtr parentHandle = parent?.Handle ?? IntPtr.Zero;
+            return (MessageBoxButton)NativeMessageBox(parentHandle, text, caption, (uint)flags);
+        }
+    }
+}
---
+++ src/Win32UI.MessageBox/MessageBoxButton.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Win32.UserInterface
+{
+    public enum MessageBoxButton
+    {
+        Abort = 3,
+        Cancel = 2,
+        Continue = 11,
+        Ignore = 5,
+        No = 7,
+        OK = 1,
+        Retry = 4,
+        TryAgain = 10,
+        Yes = 6
+    }
+}
