0903159564a7da9304eac139ac73f19f908541fe
William Kent <wjk@sunsol.me>
2016-09-22 23:10:20 -0400

    Migrate Win32UI.ImageList

---
+++ src/Win32UI.ImageList/Interop/IMAGEINFO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Win32.UserInterface.Graphics;
+
+#pragma warning disable 0649
+namespace Microsoft.Win32.UserInterface.Interop
+{
+    internal struct IMAGEINFO
+    {
+        public IntPtr hbmImage;
+        public IntPtr hbmMask;
+        public int Unused1;
+        public int Unused2;
+        public Rect rcImage;
+    }
+}
