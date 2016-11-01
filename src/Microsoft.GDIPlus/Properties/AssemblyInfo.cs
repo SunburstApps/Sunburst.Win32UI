cbf2a14fd68b9ecd77f3d5559b84db5236b41911
William Kent <wjk@sunsol.me>
2016-11-01 15:10:57 -0400

    Update Microsoft.GDIPlus project file

--- src/Microsoft.GDIPlus/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("William Kent")]
-[assembly: AssemblyCopyright("Copyright © 2016 William Kent. All rights reserved.")]
-[assembly: ComVisible(false)]
