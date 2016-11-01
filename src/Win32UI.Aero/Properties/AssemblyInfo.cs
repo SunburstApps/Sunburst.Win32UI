68d23c41832334fc2e2413261caaf82380707a45
William Kent <wjk@sunsol.me>
2016-11-01 18:04:37 -0400

    Update Win32UI.Aero project file

--- src/Win32UI.Aero/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("William Kent")]
-[assembly: AssemblyCopyright("Copyright © 2016 William Kent. All rights reserved.")]
-[assembly: ComVisible(false)]
