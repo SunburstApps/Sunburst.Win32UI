35ca091348d8f9efd3067efffa78b4454443cf54
William Kent <wjk@sunsol.me>
2016-12-22 17:39:09 -0500

    Remove NativeCallableAttribute

--- src/Win32UI.Support/NativeCallableAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace System.Runtime.InteropServices
-{
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-    public sealed class NativeCallableAttribute : Attribute
-    {
-        public string EntryPoint;
-        public CallingConvention CallingConvention;
-    }
-}
