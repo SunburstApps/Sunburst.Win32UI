0323196e4e901e1398362175061d7a6090fb2e23
William Kent <wjk@sunsol.me>
2016-12-24 17:44:23 -0500

    Initial commit of Win32UI.Theming project

---
+++ src/Win32UI.Theming/Interop/NativeMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.UserInterface.Graphics;
+
+namespace Microsoft.Win32.UserInterface.Interop
+{
+    internal static class NativeMethods
+    {
+        [DllImport("uxtheme.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr OpenThemeData(IntPtr hWnd, string themeClassList);
+
+        [DllImport("uxtheme.dll")]
+        public static extern int CloseThemeData(IntPtr hTheme);
+
+        [DllImport("uxtheme.dll")]
+        public static extern int DrawThemeBackground(IntPtr hTheme, IntPtr hDC, int partId, int startId, [In] ref Rect rect, IntPtr clipRectPtr);
+    }
+}
---
+++ src/Win32UI.Theming/ThemeData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.UserInterface.Graphics;
+using Microsoft.Win32.UserInterface.Interop;
+
+namespace Microsoft.Win32.UserInterface
+{
+    public class ThemeData : IDisposable
+    {
+        public IntPtr Handle { get; private set; }
+
+        public ThemeData(Window window, string themeClassList)
+        {
+            Handle = NativeMethods.OpenThemeData(window.Handle, themeClassList);
+            if (Handle == IntPtr.Zero) throw new System.ComponentModel.Win32Exception();
+        }
+
+        public void Dispose()
+        {
+            if (Handle != IntPtr.Zero)
+            {
+                int hr = NativeMethods.CloseThemeData(Handle);
+                if (hr != 0) Marshal.ThrowExceptionForHR(hr);
+            }
+        }
+
+        public void DrawBackground(NonOwnedGraphicsContext dc, int partId, int stateId, Rect drawRect)
+        {
+            int hr = NativeMethods.DrawThemeBackground(Handle, dc.Handle, partId, stateId, ref drawRect, IntPtr.Zero);
+            if (hr != 0) Marshal.ThrowExceptionForHR(hr);
+        }
+
+        public void DrawBackground(NonOwnedGraphicsContext dc, int partId, int stateId, Rect drawRect, Rect clipRect)
+        {
+            using (StructureBuffer<Rect> clipRectPtr = new StructureBuffer<Rect>())
+            {
+                clipRectPtr.Value = clipRect;
+                int hr = NativeMethods.DrawThemeBackground(Handle, dc.Handle, partId, stateId, ref drawRect, clipRectPtr.Handle);
+                if (hr != 0) Marshal.ThrowExceptionForHR(hr);
+            }
+        }
+    }
+}
