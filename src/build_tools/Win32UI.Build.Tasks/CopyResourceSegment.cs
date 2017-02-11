1789b3c3ee6f4a79128fcfde828f2905c7bf2767
William Kent <wjk@sunsol.me>
2017-02-10 20:31:13 -0500

    Add source for CopyResourceSegment task

---
+++ src/build_tools/Win32UI.Build.Tasks/CopyResourceSegment.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Win32UI.Build.NativeResources;
+
+namespace Win32UI.Build.Tasks
+{
+    public sealed class CopyResourceSegment : Task
+    {
+        [RequiredAttribute]
+        public ITaskItem InputFile { get; set; }
+        [RequiredAttribute]
+        public ITaskItem OutputFile { get; set; }
+
+        public override bool Execute()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Log.LogWarning("Skipping task on non-Windows platform");
+                return true;
+            }
+
+            ResourceCollection inputResources = new ResourceCollection();
+            inputResources.Load(InputFile.GetMetadata("FullPath"));
+            inputResources.Save(OutputFile.GetMetadata("FullPath"));
+            return true;
+        }
+    }
+}
