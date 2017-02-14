75e3e18bef7b1927e11b5d0d2b08d766a629c313
William Kent <wjk@sunsol.me>
2017-02-14 12:39:53 -0500

    Add Win32UI.SimpleTest project

---
+++ samples/Win32UI.SimpleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Win32.UserInterface;
+
+namespace Win32UI.SimpleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBox.Show("Inside Program.Main(), please attach debugger now.", "Win32UI Test", MessageBoxFlags.ButtonOK | MessageBoxFlags.IconInformation);
+        }
+    }
+}
