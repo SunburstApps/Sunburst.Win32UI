dff56dc8376cde76eec8210fa7ce02d958efc91d
William Kent <wjk@sunsol.me>
2017-02-15 16:21:27 -0500

    Retarget Win32UI.Build.Tasks to net46
    This change is required for an updated copy of the NativeResources
    assembly to build correctly. That copy will be committed once I
    complete porting it to .NET Core.

--- src/build_tools/Win32UI.Build.Tasks/build.ps1
+++ src/build_tools/Win32UI.Build.Tasks/build.ps1
@@ -1,7 +1,7 @@
 & dotnet restore
 & dotnet build
 
-mkdir ..\..\bin\BuildTasks\, ..\..\bin\BuildTasks\net45, ..\..\bin\BuildTasks\netcoreapp1.1 -ea 0 > $null
-copy bin\Debug\net45\Win32UI.*.dll ..\..\bin\BuildTasks\net45
+mkdir ..\..\bin\BuildTasks\, ..\..\bin\BuildTasks\net46, ..\..\bin\BuildTasks\netcoreapp1.1 -ea 0 > $null
+copy bin\Debug\net46\Win32UI.*.dll ..\..\bin\BuildTasks\net46
 copy bin\Debug\netcoreapp1.1\Win32UI.*.dll ..\..\bin\BuildTasks\netcoreapp1.1
 copy Sunburst.Win32UI.Build.targets ..\..\bin\BuildTasks
