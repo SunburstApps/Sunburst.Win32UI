3b8ed9ade07719674cd4bc5e21f5d0e7c07cebe8
William Kent <wjk@sunsol.me>
2017-02-18 19:20:30 -0500

    Finish implementing Network class

--- src/Win32UI.WindowsShell/NetworkConnections/Network.cs
+++ src/Win32UI.WindowsShell/NetworkConnections/Network.cs
@@ -5,8 +5,6 @@ namespace Microsoft.Win32.WindowsShell.NetworkConnections
 {
     public sealed class Network
     {
-        // Resume at: Network.CreatedTime (Network.cs line 99)
-
         private INetwork network;
         internal Network(INetwork iface)
         {
@@ -17,6 +15,18 @@ namespace Microsoft.Win32.WindowsShell.NetworkConnections
         {
             get => network.GetCategory();
             set => network.SetCategory(value);
+        }
+
+        public string Name
+        {
+            get => network.GetName();
+            set => network.SetName(value);
+        }
+
+        public string Description
+        {
+            get => network.GetDescription();
+            set => network.SetDescription(value);
         }
 
         public DateTime ConnectedTime
@@ -31,7 +41,23 @@ namespace Microsoft.Win32.WindowsShell.NetworkConnections
             }
         }
 
+        public DateTime CreatedTime
+        {
+            get
+            {
+                network.GetTimeCreatedAndConnected(out uint low, out uint high, out uint unused1, out uint unused2);
+                long time = high;
+                time <<= 32;
+                time |= low;
+                return DateTime.FromFileTimeUtc(time);
+            }
+        }
+
         public IEnumerable<NetworkConnection> Connections => new NetworkConnectionCollection(network.GetNetworkConnections());
         public ConnectivityState Connectiity => network.GetConnectivity();
+        public NetworkDomainType DomainType => network.GetDomainType();
+        public bool IsConnected => network.IsConnected;
+        public bool IsConnectedToInternet => network.IsConnectedToInternet;
+        public Guid NetworkId => network.GetNetworkId();
     }
 }
