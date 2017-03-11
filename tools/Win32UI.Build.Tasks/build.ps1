04ea7f88bf58c853a216a5e37a7ce921f2a90d5a
William Kent <wjk@sunsol.me>
2017-03-11 14:07:41 -0500

    Remove outdated files
    Following the repository reorganization in 3282746b35,
    these files are no longer required nor are usable.

--- tools/Win32UI.Build.Tasks/build.ps1
+++
@@ -1,7 +0,0 @@
-& dotnet restore
-& dotnet build
-
-mkdir ..\..\bin\BuildTasks\, ..\..\bin\BuildTasks\net46, ..\..\bin\BuildTasks\netcoreapp1.1 -ea 0 > $null
-copy bin\Debug\net46\Win32UI.*.dll ..\..\bin\BuildTasks\net46
-copy bin\Debug\netcoreapp1.1\Win32UI.*.dll ..\..\bin\BuildTasks\netcoreapp1.1
-copy Sunburst.Win32UI.Build.targets ..\..\bin\BuildTasks
