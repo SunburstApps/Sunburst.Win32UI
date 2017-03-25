9643e7d7fc4b09bd4c765492808cbd258c8da9a0
William Kent <wjk@sunsol.me>
2017-03-24 21:34:52 -0400

    Remove Win32UI.Native project sources
    This project is no longer used by the build process.
    I can always pull it back out of the Git history if I need it.

--- src/Win32UI.Native/ImageCodecs.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#include "dllapi.h"
-
-EXTERN_C UINATIVE_API HRESULT ImageCodecCreateBitmapFromFile(LPCWSTR filePath, HBITMAP *hBitmapPtr);
-EXTERN_C UINATIVE_API HRESULT ImageCodecCreateBitmapFromMemory(LPCVOID memory, ULONG64 size, HBITMAP *hBitmapPtr);
--- src/Win32UI.Native/dllapi.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifndef UINATIVE_API
-#ifdef  WIN32UINATIVE_EXPORTS
-#define UINATIVE_API __declspec(dllexport)
-#else
-#define UINATIVE_API __declspec(dllimport)
-#endif
-#endif
