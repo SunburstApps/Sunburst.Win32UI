0b6e69a44a12eb2b62f68ad79d8927e7a5559aaa
William Kent <wjk011@gmail.com>
2017-07-23 17:01:47 -0400

    Remove dead code

--- src/Win32UI.Graphics/Graphics/WindowPaintGraphicsContext.cs
+++ src/Win32UI.Graphics/Graphics/WindowPaintGraphicsContext.cs
@@ -5,19 +5,6 @@ namespace Microsoft.Win32.UserInterface.Graphics
 {
     public class WindowPaintGraphicsContext : NonOwnedGraphicsContext, IDisposable
     {
-        private static PAINTSTRUCT BeginPaint(Window parent)
-        {
-            PAINTSTRUCT ps = new PAINTSTRUCT();
-            NativeMethods.BeginPaint(parent.Handle, ref ps);
-            return ps;
-        }
-
-        private IntPtr ExtractHandle(PAINTSTRUCT ps)
-        {
-            PaintStruct = ps;
-            return ps.hDC;
-        }
-
         public WindowPaintGraphicsContext(Window parent)
         {
             NativeMethods.BeginPaint(parent.Handle, ref PaintStruct);
