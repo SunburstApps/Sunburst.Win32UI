2ecf8f9e5be3bb00e0e5d2f43889273850dfcaea
William Kent <wjk011@gmail.com>
2017-12-02 21:03:06 -0500

    Copy NMHDR and WindowStyles into Sunburst.WindowsForms

---
+++ src/Sunburst.WindowsForms/Interop/NMHDR.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+#pragma warning disable 0649
+namespace Sunburst.WindowsForms.Interop
+{
+    internal struct NMHDR
+    {
+        public IntPtr hWndFrom;
+        public ulong idFrom;
+        public uint code;
+    }
+}
