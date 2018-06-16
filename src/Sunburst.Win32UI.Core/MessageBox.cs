1523bc1303a4b9ff42246f8bcda972ba20a302dd
William Kent <wjk@users.noreply.github.com>
2018-06-15 22:37:32 -0400

    Fix MessageBox for use with Win32UI.Dialogs

--- src/Sunburst.Win32UI.Core/MessageBox.cs
+++ src/Sunburst.Win32UI.Core/MessageBox.cs
@@ -8,7 +8,7 @@ namespace Sunburst.Win32UI
         [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "MessageBoxW")]
         private static extern int NativeMessageBox(IntPtr hWnd, string lpText, string lpCaption, uint flags);
 
-        public static MessageBoxButton Show(string text, string caption, MessageBoxFlags flags, Control parent = null)
+        public static MessageBoxButton Show(string text, string caption, MessageBoxFlags flags, IWin32Window parent = null)
         {
             IntPtr parentHandle = parent?.Handle ?? IntPtr.Zero;
             return (MessageBoxButton)NativeMessageBox(parentHandle, text, caption, (uint)flags);
