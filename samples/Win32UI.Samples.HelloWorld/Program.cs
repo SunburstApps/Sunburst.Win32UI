6cdd0680706eca457d695aa5d2cbd5ebf7da7a86
William Kent <wjk@users.noreply.github.com>
2018-07-02 19:34:20 -0400

    Re-add samples/ directory

---
+++ samples/Win32UI.Samples.HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Sunburst.Win32UI;
+
+namespace Win32UI.Samples.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBox.Show("Hello, World!", "Win32UI Hello World", MessageBoxFlags.ButtonOK | MessageBoxFlags.IconInformation);
+        }
+    }
+}
